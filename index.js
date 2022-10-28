72cdaaad6964f87d3628cf8eae2a61c54b5754d9
mwendamwenda <benmwenda83@gmail.com>
2022-10-28 11:08:05 +0300

    final statemtsns and expressions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('kenya ia canaan that God gave us');
+// console.log('kenya ia canaan that God gave us');
+true && false && !false //this is  a expression since in the end it will produce a boolean value(expression produce results)
+
+
+// a statement in java script is a bigger piece of code that is excuted and does not produce results by itself
+
+if(23 > 10){
+    const str ='23 is greater than 10'
+    console.log(str);
+}
+
+const me = 'mwenda';
+console.log(`i am ${2022 - 1992} years old ${me}`)//this makes sense that when making literals javascript needs expressions only and not statements
